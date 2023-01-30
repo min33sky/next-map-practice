29ae242aa7e7a96a44b17bd8ad4e981824ecba4c
min33sky <min33sky@naver.com>
2023-01-30 17:11:22 +0900

    feat: font change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Nanum_Gothic } from '@next/font/google';
+
+const nanum = Nanum_Gothic({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${nanum.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
