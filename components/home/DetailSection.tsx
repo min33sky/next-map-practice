80d148640cfccbb323560db08484e904fd234062
min33sky <min33sky@naver.com>
2023-01-30 15:26:22 +0900

    feat: detail-section animation

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -1,17 +1,39 @@
 import { IoIosArrowUp } from 'react-icons/io';
 import styles from '@/styles/detail.module.scss';
+import useSWR from 'swr';
+import { CURRENT_STORE_KEY } from '@/hooks/useCurrentStore';
+import { Store } from '@/types/store';
+import { useState } from 'react';
 
 /**
  * 가게 상세 정보 표시 컴포넌트
  */
 export default function DetailSection() {
+  const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
+  const [expanded, setExpanded] = useState(false);
+
+  console.log('expanded', expanded);
+
   return (
-    <div className={styles.detailSection}>
+    <div
+      className={`${styles.detailSection} ${
+        currentStore ? styles.selected : ''
+      } ${expanded ? styles.expanded : ''}`}
+    >
       <div className={styles.header}>
-        <button className={styles.arrowButton}>
+        <button
+          className={`${styles.arrowButton} ${expanded ? styles.expanded : ''}`}
+          onClick={() => setExpanded(!expanded)}
+          disabled={!currentStore}
+        >
           <IoIosArrowUp size={20} color="#666666" />
         </button>
-        <p className={styles.title}>매장을 선택해주세요.</p>
+
+        {currentStore ? (
+          <p className={styles.title}>{currentStore.name}</p>
+        ) : (
+          <p className={styles.title}>매장을 선택해주세요.</p>
+        )}
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,14 @@ export default function Home({
   return (
     <>
       <Header />
-      <main style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        }}
+      >
         <MapSection />
         <DetailSection />
       </main>
