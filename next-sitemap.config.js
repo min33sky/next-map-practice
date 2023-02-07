d454ece3fc7a77189af93f430c216eb3db0f8f8e
min33sky <min33sky@naver.com>
2023-02-07 10:14:45 +0900

    feat: sitemap & robots.txt added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://next-map-tuto.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
