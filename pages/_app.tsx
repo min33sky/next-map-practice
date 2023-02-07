83e56e6d477514cd0cad276032094fd45955f04b
min33sky <min33sky@naver.com>
2023-02-07 11:21:35 +0900

    feat: google analytics add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { Nanum_Gothic } from '@next/font/google';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
+import Script from 'next/script';
 
 const nanum = Nanum_Gothic({
   weight: ['400', '700'],
@@ -18,6 +19,22 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <DefaultSeo {...SEO} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-4GR4WKG41N`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4GR4WKG41N');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
