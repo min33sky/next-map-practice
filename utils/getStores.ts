a168ae8e83bec1fc8bac50a474817d752b83023c
min33sky <min33sky@naver.com>
2023-02-14 19:13:32 +0900

    fix: vercel url

--- utils/getStores.ts
+++ utils/getStores.ts
@@ -2,7 +2,9 @@ import { Store } from '@/types/store';
 
 export async function getStores() {
   const data = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL || process.env.VERCEL_URL}/api/stores`
+    `${
+      process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_VERCEL_URL
+    }/api/stores`
   ).then((res) => res.json());
 
   return data;
