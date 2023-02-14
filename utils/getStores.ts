e17397b167686460b1618a61c0aa284eaa0b04a8
min33sky <min33sky@naver.com>
2023-02-14 19:16:15 +0900

    fix: vercel 2

--- utils/getStores.ts
+++ utils/getStores.ts
@@ -1,11 +1,11 @@
 import { Store } from '@/types/store';
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_API_URL ||
+  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
 export async function getStores() {
-  const data = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-    }/api/stores`
-  ).then((res) => res.json());
+  const data = await fetch(`${baseUrl}/api/stores`).then((res) => res.json());
 
   return data;
 }
