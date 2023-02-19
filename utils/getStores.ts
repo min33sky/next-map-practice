720639a2d45a3c27f7cb31870f374b084b8513a2
min33sky <min33sky@naver.com>
2023-02-19 21:50:58 +0900

    vercel fix

--- utils/getStores.ts
+++ utils/getStores.ts
@@ -1,11 +1,18 @@
 import { Store } from '@/types/store';
 
 const baseUrl =
-  process.env.NEXT_PUBLIC_API_URL ||
+  process.env.NEXT_PUBLIC_API_URL ??
   `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export async function getStores() {
-  const data = await fetch(`${baseUrl}/api/stores`).then((res) => res.json());
+  const data = await fetch(`${baseUrl}/api/stores`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => res.json());
+
+  console.log('베이스유알엘::::: ', baseUrl);
+  console.log('##### API 불러오기: ', data);
 
   return data;
 }
